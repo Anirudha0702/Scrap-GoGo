907384db33c52c436d7b175e8560b3f56f67503e
ANIRUDHA PRADHAN <114860976+Anirudha0702@users.noreply.github.com>
2025-06-21 14:58:42 +0530

    style: improve code formatting and consistency in Search.ts

--- src/controllers/Search.ts
+++ src/controllers/Search.ts
@@ -1,23 +1,23 @@
 import { Request, Response } from "express";
-import axios ,{AxiosError}from "axios";
-import {  IAnimeResult } from "../types";
+import axios, { AxiosError } from "axios";
+import { IAnimeResult } from "../types";
 import * as cheerio from "cheerio";
-export default async (req: Request, res: Response)=> {
+export default async (req: Request, res: Response) => {
   const { keyword, page } = req.query;
-  if(!keyword ) return res.status(400).json(
-    {
-      success:false,
-      statusCode:400,
-      error:"keyword is required",
-      data:null
+  if (!keyword)
+    return res.status(400).json({
+      success: false,
+      statusCode: 400,
+      error: "keyword is required",
+      data: null,
     });
 
   const baseURL = process.env.BASE_URL;
-  const url = `${baseURL}/filter.html?keyword=${keyword}&page=${page}`;
+  const url = `${baseURL}?s=${keyword}`;
   try {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-    const searchResult: IAnimeResult[] = [];  
+    const searchResult: IAnimeResult[] = [];
     const animes = $("div.last_episodes ul.items>li");
     animes.each((index, element) => {
       const anime: IAnimeResult = {
@@ -29,17 +29,19 @@ export default async (req: Request, res: Response)=> {
     });
     return res.json({
       success: true,
-      statusCode:200,
-      error:null,
-      data:searchResult,
-
+      statusCode: 200,
+      error: null,
+      data: searchResult,
     });
-  } catch (error:any  ) {
-      return res.status(error.code||500).json({
-        success: false,
-        statusCode:error.code||500,
-        error: error instanceof AxiosError?"Gogo Anime is down or may have changed is domain":error.message || "Internal Server Error",
-        data:null
-      })
-    }
+  } catch (error: any) {
+    return res.status(error.code || 500).json({
+      success: false,
+      statusCode: error.code || 500,
+      error:
+        error instanceof AxiosError
+          ? "Gogo Anime is down or may have changed is domain"
+          : error.message || "Internal Server Error",
+      data: null,
+    });
+  }
 };
